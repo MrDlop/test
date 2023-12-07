ecfe4599b408f7008f35a359a775439288307a42
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-12-07 13:20:49 +0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,86 @@
+lang = "ru"
+bot_answer = {
+    "ru": {
+        "start_ok": "Для получения информации введите /help",
+        "not_authorization": "Для входа введите /authorization",
+        "authorization_name": "Введите название компании",
+        "authorization_password": "Введите пароль",
+        "authorization_success": "Удачная авторизация",
+        "authorization_fail": "Неверный пароль или название компании",
+        "authorization_fail_1": "Ограничение на количество людей",
+        "logout_success": "Успешный выход",
+        "prompt": "Выберите настройку",
+        "incorrect_prompt": "Этой настройки не существует",
+        "success_prompt": "Настройка успешно установлена",
+        "choose_company": "Выберете компанию",
+        "unreal_company": "Данной компании не существует",
+        "confirm": "Подтвердить",
+        "ok": "ОК",
+        "input_name_company": "Введите название компании",
+        "input_password": "Введите пароль",
+        "input_time": "Введите дату окончания подписки дд.мм.гг",
+        "input_num_of_user": "Введите максимальное количество пользователей",
+        "input_incorrect": "Неправильный ввод данных",
+        "end_time": "Время подписки истекло",
+        "input_prompt": "Введите текст подсказки",
+        "input_description": "Введите описание",
+        "help": """/authorization - авторизация пользователя
+/help - получение справки
+/settings - настройка режима
+/log_out - выход из аккаунта
+любое другое сообщение - получение ответа от нейросети
+""",
+        "help_admin": """/authorization - авторизация пользователя
+/help - получение справки
+/settings - настройка режима
+/log_out - выход из аккаунта
+/delete - удаление компании
+/add - добавление компании
+/update_limit - изменение максимального количества пользователей от компании
+/update_date - изменение срока окончания подписки
+/add_prompt - добавление шаблона для нейросети
+любое другое сообщение - получение ответа от нейросети
+"""
+    },
+    "en": {
+        "start_ok": "For information, type /help",
+        "not_authorization": "To log in, enter /authorization",
+        "authorization_name": "Enter the company name",
+        "authorization_password": "Enter password",
+        "authorization_success": "Authorization was successful",
+        "logout_success": "Log out was successful",
+        "authorization_fail": "Incorrect password or company name",
+        "authorization_fail_1": "Limit on the number of people",
+        "prompt": "Choose settings",
+        "incorrect_prompt": "This setting does not exist",
+        "success_prompt": "The setting has been successfully installed",
+        "choose_company": "Choose company",
+        "unreal_company": "This company does not exist",
+        "confirm": "Confirm",
+        "ok": "OK",
+        "input_name_company": "Enter the company name",
+        "input_password": "Enter password",
+        "input_time": "Enter subscription end date dd.mm.yy",
+        "input_num_of_user": "Enter the maximum number of users",
+        "input_incorrect": "Incorrect data entry",
+        "end_time": "Subscription time has expired",
+        "input_prompt": "Enter text of prompt",
+        "input_description": "Enter a description",
+        "help": """/authorization - user authorization
+/help - getting help
+/settings - setting the mode
+/log_out - log out of the account
+any other message - receiving a response from the neural network
+        """,
+        "help_admin": """/authorization - user authorization
+/help - getting help
+/settings - setting the mode
+/log_out - log out of the account
+/delete - deleting a company
+/add - adding a company
+/update_limit - change the maximum number of users from the company
+/update_date - change the subscription expiration date
+/add_prompt - adding a template for a neural network
+any other message - receiving a response from the neural network
+"""
+    }}
