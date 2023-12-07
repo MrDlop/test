d54c7294af1e705f6e0d466cbf92b20e4b605b26
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-12-07 13:50:09 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "6223135936:AAHj7gpcKVi36TG4bIZ4S971qzUlQNtJxeM"
+API_KEY = [
+    "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",
+    "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",
+    "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",
+    "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",
+    "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"
+]
